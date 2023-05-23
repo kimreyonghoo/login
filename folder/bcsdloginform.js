8d51d188e859e2336577fdf0b1f389003a3e608d
kimreyonghoo <krh6818@naver.com>
2023-05-23 15:09:38 +0900

    $ git commit --allow-empty-message -m ""

--- folder/bcsdloginform.js
+++ folder/bcsdloginform.js
@@ -3,15 +3,26 @@ logintry=()=>{
 }
 const $img =document.getElementById("cat");
 const $getcat=document.getElementById("get-cat");
-    
-    function getcatimg(){
-      fetch("https://api.thecatapi.com/v1/images/search?size=full").then(//json 객체를 가져오기
-        (response)=>{
-          response.json().then((res) => {
-            $img.src=res[0].url;
-            console.log (response);
-          });
-        }
-      );
+const $id=document.getElementsByClassName("idinput");
+function getcatimg(){
+  fetch("https://api.thecatapi.com/v1/images/search?size=full").then(//json 객체를 가져오기
+    (response)=>{
+      response.json().then((res) => {
+        $img.src=res[0].url;
+        console.log (response);
+      });
     }
-    $getcat.addEventListener("click",getcatimg);
+  );
+}
+$getcat.addEventListener("click",getcatimg);
+function login(){
+  let cookieid=$id[0].value;
+  let exTime=new Date();
+  exTime.setTime(exTime.getHours()+72);
+  document.cookie=`id=${cookieid};expires=${exTime}`;
+  localStorage.setItem("id", cookieid);
+  console.log(`${cookieid}`)
+}
+const $loginbtn=document.getElementById("loginbtn");
+$loginbtn.addEventListener("click",login);
+
